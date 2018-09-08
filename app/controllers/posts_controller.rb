c3b7dededfca04e66872f24892c0c3377eeb8e3f
Samantha Helene Thomas <2shthomas@gmail.com>
2018-09-08 11:07:35 -0600

    devise

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,30 @@
 class PostsController < ApplicationController
   def index
+    @post =Post.all
   end
 
   def show
   end
 
   def new
+    @post =Post.new
   end
 
   def edit
+    @post =Post.find(params[:id])
   end
 
   def destroy
+    Post.find(params[:id]).destroy
   end
 
   def create
+    Post.create(post_params)
   end
 
   def update
+    post.find(params[:id]).update(post_params)
   end
+
+
 end
