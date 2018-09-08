4983382ce07b2489affc08284e51f431e770aa8b
Austin McCombs <austinmccombs22@gmail.com>
2018-09-08 11:52:34 -0600

    Initial devise stuff

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,10 @@
 class ApplicationRecord < ActiveRecord::Base
-  self.abstract_class = true
+ #before_action :configure_permitted_paramaters, if: :devise_controller?
+
+ private
+
+ def configure_permitted_paramaters
+  devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+ end
+ 
 end
