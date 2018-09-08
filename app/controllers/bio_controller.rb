5b67ba2225596f53b6179335ca23b3ea530d4cf2
Katrina Tran <kat.c.tran@gmail.com>
2018-09-08 11:52:31 -0600

    updated controllers

--- app/controllers/bio_controller.rb
+++ app/controllers/bio_controller.rb
@@ -1,20 +1,27 @@
 class BioController < ApplicationController
   def index
+    @bios = Bio.all
   end
 
   def show
   end
 
   def new
+    @bio = Bio.new
   end
 
   def edit
+    @bio = Bio(params[:id])
   end
 
   def destroy
+    bio = Bio.find(params[:id]).destroy
   end
 
   def create
+    # @bio.create(bio_params)
+    @bio = Bio.new(bio_params)
+    
   end
 
   def update
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,22 +1,41 @@
 class CommentsController < ApplicationController
   def index
+    @comments = Comment.all
   end
 
   def show
+    @comment = Comment.find(params[:id])
   end
 
   def new
+    @comment = Comment.new
   end
 
   def edit
+    @comment = Comment.find(params[:id])
   end
 
   def destroy
+    Comment.find(params[:id]).destroy
   end
 
   def create
+    @comment = Comment.create(comment_params)
+    if @comment.save
+      redirect_to comments_path
   end
 
   def update
+    Comment.find(params[:id]).update(comment_params)
+  end
+
+  private
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:message, :post_id)
   end
 end
