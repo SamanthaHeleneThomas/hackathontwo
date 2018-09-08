5a49e7fe432a4cf80c90bcfe13cd8a7b63b62d10
Samantha Helene Thomas <2shthomas@gmail.com>
2018-09-08 11:52:35 -0600

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,3 +10,63 @@ Rails.application.routes.draw do
   resources :comments
 
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# get 'comments/index'
+# get 'comments/show'
+# get 'comments/new'
+# get 'comments/edit'
+# get 'comments/destroy'
+# get 'comments/create'
+# get 'comments/update'
+# get 'bio/index'
+# get 'bio/show'
+# get 'bio/new'
+# get 'bio/edit'
+# get 'bio/destroy'
+# get 'bio/create'
+# get 'bio/update'
+# get 'posts/index' 
+# get 'posts/show'
+# get 'posts/new'
+# get 'posts/edit'
+# get 'posts/destroy'
+# get 'posts/create'
+# get 'posts/update'
+# get 'user/index'
+# get 'user/show'
+# get 'user/new'
+# get 'user/edit'
+# get 'user/destroy'
+# get 'user/create'
+# get 'user/update'
